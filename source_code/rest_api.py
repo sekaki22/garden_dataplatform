2844e55cdaebaf9151ea7df16a6eccd958d2786b
selimberntsen <selimbernt@hotmail.com>
2024-08-17 00:07:48 +0200

    Added post request

--- source_code/rest_api.py
+++ source_code/rest_api.py
@@ -4,15 +4,23 @@ from constants import DATABASE
 from helpers import fetch_database
 import sqlite3
 import json
-
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
+
+class DHT22_Model(BaseModel):
+    temperature: float
+    humidity: float
+
 
 # Get data from sqlite db
 @app.get('/sensors/dht_22')
 def get_dht22(from_date:str):
     """ 
-    from_date: Get the data from a specific date untill nowi 
+    from_date: Get the data from a specific date untill now
+    
+    returns: json data dump 
     """
     # Create a cursor
     conn = sqlite3.connect(DATABASE)
@@ -26,4 +34,30 @@ def get_dht22(from_date:str):
     results = fetch_database(cursor, query)
 
     return json.dumps(results, indent=4)
+
+# Post data to sqlite db
+@app.post('/sensors/dht_22')
+def post_dht22(instance: DHT22_Model):
+    """
+    """    
     
+    # Create a cursor
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    
+    query = f"""
+            INSERT INTO dht_22 (date_time, temp_c, humidity_perc)
+            VALUES (?, ?, ?)
+            """ 
+   
+    # Create database input
+    datetime_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    database_input = (datetime_now, instance.temperature, 
+                            instance.humidity)
+    # Execute and commit :)
+    cursor.execute(query, database_input)
+    conn.commit() 
+    
+    return "200: Data was ingested into database"
+
+     
