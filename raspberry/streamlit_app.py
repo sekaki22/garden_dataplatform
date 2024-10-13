c366dd679ea1a71e1c80f521e039aea9cf9277da
Selim Berntsen <selimbernt@hotmail.com>
2024-10-13 23:35:06 +0200

    improve datetime sliders

--- raspberry/streamlit_app.py
+++ raspberry/streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 from constants import DATABASE
-from helpers import plot_timeseries
-import plotly.express as px 
+from helpers import plot_timeseries_with_thresholds
+import plotly.express as px
 from datetime import datetime, timedelta
-
-# Add streamlit date picker for since and untill date
-since_date = st.date_input("Since date", value=datetime.now())
-untill_date = st.date_input("Until date", value=datetime.now())
 
 st.title("Kelly's garden dashboard")
 
@@ -17,62 +13,51 @@ st.write("""
 
 col1, col2 = st.columns(2)
 
+# Place the widgets above the plot to avoid reloading the plot when the slider is moved     
 with col1:
-    since_temp = st.date_input(
-        "Since date for Temperature",
-        value=datetime.now() - timedelta(hours=1),
-        key="since_temp"
+    # Datetime range slider for temperature
+    since_temp, until_temp = st.slider(
+        "Select datetime range for Temperature",
+        value=(datetime.now() - timedelta(hours=1), datetime.now()),
+        format="MM/DD/YY - HH:mm",
+        key="temp_datetime_range"
     )
-    until_temp = st.date_input(
-        "Until date for Temperature",
-        value=datetime.now(),
-        key="until_temp"
-    )
-    fig_temp = plot_timeseries("dht_22", DATABASE, "temp_c", since_temp, until_temp)
+    fig_temp = plot_timeseries_with_thresholds("dht_22", DATABASE, "temperature in Celsius", since_temp, until_temp)
     st.plotly_chart(fig_temp)
 
-    # Date range selector for humidity
-    since_hum = st.date_input(
-        "Since date for Humidity",
-        value=datetime.now() - timedelta(hours=1),
-        key="since_hum"
+    since_hum, until_hum = st.slider(
+    "Select datetime range for Humidity",
+    value=(datetime.now() - timedelta(hours=1), datetime.now()),
+    format="MM/DD/YY - HH:mm",
+    key="hum_datetime_range"
     )
-    until_hum = st.date_input(
-        "Until date for Humidity",
-        value=datetime.now(),
-        key="until_hum"
-    )
-    # Fill in proper since and untill parameters
-    fig_hum = plot_timeseries("dht_22", DATABASE, "humidity_perc", since_hum, until_hum)
+    
+    # Datetime range slider for humidit
+    fig_hum = plot_timeseries_with_thresholds("dht_22", DATABASE, "humidity %", 100, "threshold.json", (since_hum, until_hum))
     st.plotly_chart(fig_hum)
 
+
+
 with col2:
-    # Date range selector for lux
-    since_lux = st.date_input(
-        "Since date for Lux",
-        value=datetime.now() - timedelta(hours=1),
-        key="since_lux"
-    )
-    until_lux = st.date_input(
-        "Until date for Lux",
-        value=datetime.now(),
-        key="until_lux"
-    )
-    fig_lux = plot_timeseries("tsl_2591", DATABASE, "lux", since_lux, until_lux)
+    # Datetime range slider for lux
+    since_lux, until_lux = st.slider(
+    "Select datetime range for Lux",
+    value=(datetime.now() - timedelta(hours=1), datetime.now()),
+    format="MM/DD/YY - HH:mm",
+    key="lux_datetime_range"
+)
+
+    fig_lux = plot_timeseries_with_thresholds("tsl_2591", DATABASE, "lux", 60, "threshold.json", (since_lux, until_lux))
     st.plotly_chart(fig_lux)
-    
-    # Date range selector for soil moisture
-    since_soil = st.date_input(
-        "Since date for Soil Moisture",
-        value=datetime.now() - timedelta(hours=1),
-        key="since_soil"
-    )
-    until_soil = st.date_input(
-        "Until date for Soil Moisture",
-        value=datetime.now(),
-        key="until_soil"
-    )
-    fig_soil = plot_timeseries("soil_moisture_sens", DATABASE, "raw_value", since_soil, until_soil)
+
+    since_soil, until_soil = st.slider(
+    "Select datetime range for Soil Moisture",
+    value=(datetime.now() - timedelta(hours=1), datetime.now()),
+    format="MM/DD/YY - HH:mm",
+    key="soil_datetime_range"
+)
+    # Datetime range slider for soil moistur
+    fig_soil = plot_timeseries("soil_moisture_sens", DATABASE, "milli voltage", since_soil, until_soil)
     st.plotly_chart(fig_soil)
 
 
